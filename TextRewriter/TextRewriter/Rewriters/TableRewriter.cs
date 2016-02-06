eebe0c4b60dc5fdbf4c3f009bdf9aa03d9dd6316
evewiz <ewelina.szulc@hotmail.com>
2016-02-06 23:04:24 +0100

    Add TableRewriter logic and tests

--- TextRewriter/TextRewriter/Rewriters/TableRewriter.cs
+++ TextRewriter/TextRewriter/Rewriters/TableRewriter.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Text;
 using TextRewriter.Splitters;
 
 namespace TextRewriter.Rewriters
@@ -16,7 +16,25 @@ namespace TextRewriter.Rewriters
 
         public string Rewrite(string text)
         {
-            throw new NotImplementedException();
+            StringBuilder builder = new StringBuilder();
+            string[] rows = rowSplitter.Split(text);
+
+            if (rows.Length == 0)
+            {
+                return null;
+            }
+
+            string[] columnsHeader = columnSplitter.Split(rows[0]);
+            for (int i = 1; i < rows.Length; i++)
+            {
+                string[] columns = columnSplitter.Split(rows[i]);
+                for (int j = 0; j < columns.Length && j < columnsHeader.Length; j++)
+                {
+                    builder.AppendLine($"{columnsHeader[j].Trim()}: {columns[j]}");
+                }
+            }
+
+            return builder.ToString();
         }
     }
 }
---
+++ TextRewriter/TextRewriterTest/Rewriters/TableRewriterTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using TextRewriter.Rewriters;
+using TextRewriter.Splitters;
+using TextRewriterTest.Properties;
+
+namespace TextRewriterTest.Rewriters
+{
+    [TestFixture]
+    public class TableRewriterTest
+    {
+        [Test, TestCaseSource(typeof(TableRewriterTestCasesFactory), nameof(TableRewriterTestCasesFactory.TestCases))]
+        public string RewriteTest(string text)
+        {
+            ITextSplitter rowSplitter = new NewLineSplitter();
+            ITextSplitter columnSplitter = new TabSplitter();
+            ITextRewriter target = new TableRewriter(rowSplitter, columnSplitter);
+            return target.Rewrite(text);
+        }
+    }
+
+    public class TableRewriterTestCasesFactory
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(Resources.TableRewriterTest1Text).Returns(Resources.TableRewriterTest1Expected);
+                yield return new TestCaseData(Resources.TableRewriterTest2Text).Returns(Resources.TableRewriterTest2Expected);
+            }
+        }
+    }
+}
